e629ddbd74755788145a4d5742e2cba77293d49f
Abdulwedud <abdulwedudyassin2@gmail.com>
2025-06-12 11:31:27 +0300

    backgroung.jpg correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "custom-bg": "url('./images/background.jpg')",
+        "custom-bg": "url('/images/background.jpg')",
       },
     },
   },
